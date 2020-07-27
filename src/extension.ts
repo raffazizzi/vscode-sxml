db38aaddb0b14c425fc947a344f6cf1f68bbdae3
raffazizzi <raffaeleviglianti@gmail.com>
2020-07-27 18:16:47 -0400

    switched to salve annos and fixed dot in XML Name regex

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,20 @@ import SalveCompletionProvider from './completion';
 import 'cross-fetch/polyfill';
 import * as url from 'url';
 import * as path from 'path';
-import {Grammar, convertRNGToPattern, DefaultNameResolver, Name} from 'salve';
+import {Grammar, convertRNGToPattern, DefaultNameResolver, Name} from 'salve-annos';
 import * as fileUrl from "file-url";
 import { SaxesParser, SaxesTag, SaxesAttributeNS } from "saxes";
+import SalveDocumentationProvider from './hover';
 
 const ERR_VALID = 'ERR_VALID';
 const ERR_WELLFORM = 'ERR_WELLFORM';
 const ERR_SCHEMA = 'ERR_SCHEMA';
 const NO_ERR = 'NO_ERR';
 
-// XML Name regex (minus : and  [#x10000-#xEFFFF] range)
+// XML Name regex (minus : and [#x10000-#xEFFFF] range)
 const nameStartChar = new RegExp(/_|[A-Z]|[a-z]|[\u00C0-\u00D6]|[\u00D8-\u00F6]|[\u00F8-\u02FF]|[\u0370-\u037D]|[\u037F-\u1FFF]|[\u200C-\u200D]|[\u2070-\u218F]|[\u2C00-\u2FEF]|[\u3001-\uD7FF]|[\uF900-\uFDCF]|[\uFDF0-\uFFFD]/);
-const nameChar = new RegExp(`${nameStartChar.source}|-|.|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]`);
-const XMLname = new RegExp(`(${nameStartChar.source})(${nameChar.source})*`);
+const nameChar = new RegExp(`${nameStartChar.source}|-|\\.|[0-9]|\u00B7|[\u0300-\u036F]|[\u203F-\u2040]`);
+const XMLname = new RegExp(`^(${nameStartChar.source})(${nameChar.source})*$`);
 
 export interface StoredGrammar {
   rngURI?: string;
